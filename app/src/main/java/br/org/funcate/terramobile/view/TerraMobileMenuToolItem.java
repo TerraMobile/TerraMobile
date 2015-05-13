ee469e67a099e307a9021801ab22570de9b0a0f8
André Fernando A. Carvalho <afacarvalho@yahoo.com.br>
2015-05-13 12:16:58 -0300

    Adding new tool Ids on model

--- app/src/main/java/br/org/funcate/terramobile/view/TerraMobileMenuToolItem.java
+++ app/src/main/java/br/org/funcate/terramobile/view/TerraMobileMenuToolItem.java
@@ -5,13 +5,26 @@ package br.org.funcate.terramobile.view;
  */
 public class TerraMobileMenuToolItem extends TerraMobileMenuItem {
 
+    // this values should be the same order on string.xml
     public static final int DOWNLOAD_GPKG=0;
     public static final int CREATE_GPKG=1;
     public static final int READ_GEOM=2;
     public static final int INSERT_DATA=3;
+    public static final int BTN_TEST=4;
 
-    public TerraMobileMenuToolItem(String label) {
+    private int toolID;
+
+    public TerraMobileMenuToolItem(String label,int tool) {
         this.setLabel(label);
         this.setType(TOOL_ITEM);
+        this.toolID=tool;
+    }
+
+    public int getToolID() {
+        return toolID;
+    }
+
+    public void setToolID(int toolID) {
+        this.toolID = toolID;
     }
 }
