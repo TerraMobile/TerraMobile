8cbb2978bd22630f7894d7cb297950af0230238c
Claudio Bogossian <claudio.bogossian@gmail.com>
2015-06-18 15:01:12 -0300

    Removing the zoom controls. Fixes #8

--- app/src/main/java/br/org/funcate/terramobile/controller/activity/MenuMapController.java
+++ app/src/main/java/br/org/funcate/terramobile/controller/activity/MenuMapController.java
@@ -59,14 +59,6 @@ public class MenuMapController {
             if(child.getOsmOverLayer()==null)
             {
                 MapView mapView = (MapView) ((MainActivity) context).findViewById(R.id.mapview);
-                mapView.setMaxZoomLevel(18);
-                mapView.setBuiltInZoomControls(true);
-                mapView.setMultiTouchControls(true);
-
-                System.out.println("Overlay size:" + mapView.getOverlayManager().size());
-
-/*        OnlineTileSourceBase mapQuestTileSource = TileSourceFactory.MAPQUESTOSM;
-        String tileSourcePath = mapQuestTileSource.OSMDROID_PATH.getAbsolutePath() + "/";*/
 
                 final MapTileProviderBasic tileProvider = new MapTileProviderBasic(context);
 
@@ -74,19 +66,13 @@ public class MenuMapController {
                 MapTileModuleProviderBase moduleProvider = new MapTileGeoPackageProvider(tileSource, child.getName(), child.getGeoPackage());
                 SimpleRegisterReceiver simpleReceiver = new SimpleRegisterReceiver(context);
 
-                //MapTileProviderArray tileProviderArray = new MapTileProviderArray(tileSource, simpleReceiver, new MapTileModuleProviderBase[] { moduleProvider });
                 MapTileProviderArray tileProviderArray = new MapTileProviderArrayGeoPackage(tileSource, simpleReceiver, new MapTileModuleProviderBase[] { moduleProvider }, ((MainActivity) this.context).getMapFragment());
-/*        tileProvider.setTileSource(tileSource);*/
+
                 final TilesOverlay tilesOverlay = new TilesOverlay(tileProviderArray, context);
                 tilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
                 mapView.getOverlays().add(INDEX_BASE_LAYER,tilesOverlay);
                 child.setOsmOverLayer(tilesOverlay);
-/*
-            this.lastIndexDrawOrder++;
-            child.setIndexOverlay(this.lastIndexDrawOrder);
-*/
-                //mapView.getOverlayManager().overlaysReversed();
-                //mapView.getTileProvider().clearTileCache();
+
                 tileProvider.setTileRequestCompleteHandler(new SimpleInvalidationHandler(mapView));
                 mapView.setTileSource(tileSource);
                 mapView.setUseDataConnection(false); //  letting osmdroid know you would use it in offline mode, keeps the mapView from loading online tiles using network connection.*/
