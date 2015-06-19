b99b4434afe143f221e2a021520e0b4b03d309f2
André Fernando A. Carvalho <afacarvalho@yahoo.com.br>
2015-06-19 15:32:17 -0300

    Adding "drop table" on sql script.

--- dynamicforms/src/db.scripts/formModel.sql
+++ dynamicforms/src/db.scripts/formModel.sql
@@ -1,32 +1,16 @@
-CREATE TABLE IF NOT EXISTS tm_layer_form (
-tm_conf_id INTEGER PRIMARY KEY AUTOINCREMENT,
-gpkg_layer_identify TEXT NOT NULL,
-tm_form TEXT,
-CONSTRAINT fk_layer_identify_id FOREIGN KEY (gpkg_layer_identify) REFERENCES gpkg_contents(table_name)
+DROP TABLE IF EXISTS "tm_layer_form";
+-- Describe TM_LAYER_FORM
+CREATE TABLE IF NOT EXISTS "tm_layer_form" (
+"tm_conf_id" INTEGER PRIMARY KEY AUTOINCREMENT,
+"gpkg_layer_identify" TEXT NOT NULL,
+"tm_form" TEXT,
+CONSTRAINT fk_layer_identify_id FOREIGN KEY ("gpkg_layer_identify") REFERENCES gpkg_contents(table_name)
 );
 
--- Describe FIELD_WORK_COLLECT_DATA
-CREATE TABLE "field_work_collect_data" (
-"field_work_id" INTEGER PRIMARY KEY AUTOINCREMENT,
-"location_name" TEXT,
-"temperature" DOUBLE,
-"soil_ph" INTEGER,
-"collect_date" DATE,
-"termites" INTEGER,
-"crop_stage" TEXT,
-"picture" BLOB,
-"geometry" BLOB);
-
--- Describe FIELD_WORK_PICTURE_DATA
-CREATE TABLE "field_work_picture_data" (
-"PK_UID" INTEGER PRIMARY KEY AUTOINCREMENT,
-"field_work_id" INTEGER NOT NULL,
-"picture" BLOB,
-"picture_mime_type" TEXT,
-CONSTRAINT fk_field_work_id FOREIGN KEY (field_work_id) REFERENCES field_work_collect_data(field_work_id));
-
+DELETE FROM gpkg_geometry_columns WHERE table_name='field_work_collect_data';
 INSERT INTO gpkg_geometry_columns ("geometry_type_name", "m", "z", "srs_id", "column_name", "table_name") values('POINT', '0', '0', '4326', 'geometry', 'field_work_collect_data');
 
+DELETE FROM gpkg_contents WHERE table_name='field_work_collect_data';
 INSERT INTO gpkg_contents ("table_name", "data_type", "identifier", "description", "last_change", "min_x", "min_y", "max_x", "max_y", "srs_id") values('field_work_collect_data', 'features', 'field_work_collect_data', 'field_work_collect_data', strftime('%Y-%m-%dT%H:%M:%fZ','now','localtime'), -180.000000, -90.000000, 180.000000, 90.000000, 4326);
 
 INSERT INTO tm_layer_form ("gpkg_layer_identify", "tm_form" )
@@ -86,3 +70,25 @@ values('field_work_collect_data','[{
         ]
     }
 ]');
+
+DROP TABLE IF EXISTS  "field_work_collect_data";
+-- Describe FIELD_WORK_COLLECT_DATA
+CREATE TABLE IF NOT EXISTS "field_work_collect_data" (
+"field_work_id" INTEGER PRIMARY KEY AUTOINCREMENT,
+"location_name" TEXT,
+"temperature" DOUBLE,
+"soil_ph" INTEGER,
+"collect_date" DATE,
+"termites" INTEGER,
+"crop_stage" TEXT,
+"picture" BLOB,
+"geometry" BLOB);
+
+DROP TABLE IF EXISTS "field_work_picture_data";
+-- Describe FIELD_WORK_PICTURE_DATA
+CREATE TABLE IF NOT EXISTS "field_work_picture_data" (
+"PK_UID" INTEGER PRIMARY KEY AUTOINCREMENT,
+"field_work_id" INTEGER NOT NULL,
+"picture" BLOB,
+"picture_mime_type" TEXT,
+CONSTRAINT fk_field_work_id FOREIGN KEY (field_work_id) REFERENCES field_work_collect_data(field_work_id));
