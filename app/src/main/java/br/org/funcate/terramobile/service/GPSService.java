0cfa582976cab0c1380b9734bfa83826a118406d
André Fernando A. Carvalho <afacarvalho@yahoo.com.br>
2015-08-06 14:40:49 -0300

    Fix #25

--- app/src/main/java/br/org/funcate/terramobile/service/GPSService.java
+++ app/src/main/java/br/org/funcate/terramobile/service/GPSService.java
@@ -1,15 +1,57 @@
 package br.org.funcate.terramobile.service;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.location.LocationListener;
 import android.location.LocationManager;
+
+import org.osmdroid.util.GeoPoint;
 
 /**
  * Created by Andre Carvalho on 04/08/15.
  */
 public class GPSService {
 
-    public static boolean isEnable(Context context) {
-        LocationManager mlocManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        return mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    // The minimum distance to change Updates in meters
+    private static long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 0 meters
+
+    // The minimum time between updates in milliseconds
+    private static long MIN_TIME_BW_UPDATES = 0; // 0 minute (ex.: to 1 minute use (1000 * 60 * 1))
+
+    public static boolean isGPSProviderEnable(Context context) {
+        return GPSService.getLocationManager(context).isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    public static boolean isNetworkProviderEnable(Context context) {
+        return GPSService.getLocationManager(context).isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    public static boolean registerListener(Context context, LocationListener locationListener) {
+        if(GPSService.isNetworkProviderEnable(context)){
+            GPSService.getLocationManager(context).requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    GPSService.MIN_TIME_BW_UPDATES,
+                    GPSService.MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    locationListener);
+            return true;
+        }else if (GPSService.isGPSProviderEnable(context)) {
+            GPSService.getLocationManager(context).requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    GPSService.MIN_TIME_BW_UPDATES,
+                    GPSService.MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    locationListener);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void unregisterListener(Context context, LocationListener locationListener){
+        // Remove the listener you previously added
+        GPSService.getLocationManager(context).removeUpdates(locationListener);
+    }
+
+    private static LocationManager getLocationManager(Context context) {
+        return (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
     }
 }
