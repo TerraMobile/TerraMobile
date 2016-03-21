e0be46432d9fe7bb87cc53e8b7e19c5028836ffe
andre.carvalho <andre.carvalho@funcate.org.br>
2016-03-21 09:08:05 -0300

    Feedback of upload file

--- app/src/main/java/br/org/funcate/terramobile/controller/activity/tasks/UploadTask.java
+++ app/src/main/java/br/org/funcate/terramobile/controller/activity/tasks/UploadTask.java
@@ -3,9 +3,7 @@ package br.org.funcate.terramobile.controller.activity.tasks;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -20,27 +18,12 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
 
 import br.org.funcate.terramobile.R;
 import br.org.funcate.terramobile.controller.activity.MainActivity;
 import br.org.funcate.terramobile.util.Message;
-import br.org.funcate.terramobile.util.Util;
 
 /**
  * This AsyncTask upload a geopackage from the server
@@ -54,9 +37,6 @@ public class UploadTask extends AsyncTask<String, String, Boolean> {
 
     private File originFile;
 
-    // creates a unique boundary based on time stamp
-    String boundary = "===" + System.currentTimeMillis() + "===";
-
     public UploadTask(String uploadOriginFilePath, MainActivity mainActivity) {
         this.mainActivity = mainActivity;
         this.uploadOriginFilePath = uploadOriginFilePath;
@@ -67,7 +47,8 @@ public class UploadTask extends AsyncTask<String, String, Boolean> {
         mainActivity.showLoadingDialog(mainActivity.getString(R.string.send_project_upload));
     }
 
-    public void onPostExecute() {
+    @Override
+    protected void onPostExecute(Boolean result) {
         try {
             mainActivity.getProgressDialog().dismiss();
         }catch (Exception e) {
@@ -106,13 +87,13 @@ public class UploadTask extends AsyncTask<String, String, Boolean> {
 
             HttpPost httpPost = new HttpPost(urlToUpload[0]);
 
-            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-
             MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create().setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
             entityBuilder.addPart("user", new StringBody("userName", ContentType.TEXT_PLAIN));
 
             entityBuilder.addPart("password", new StringBody("password", ContentType.TEXT_PLAIN));
+
+            entityBuilder.addPart("fileName", new StringBody(originFile.getName(), ContentType.TEXT_PLAIN));
 
             entityBuilder.addPart("file", new FileBody(originFile));
 
@@ -126,42 +107,12 @@ public class UploadTask extends AsyncTask<String, String, Boolean> {
 
             if (statusCode == 200) {
                 return true;
-
             }
             else
             {
                 return false;
             }
 
-           /* URL url = new URL(urlToUpload[0]);
-            HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
-            urlConnection.setConnectTimeout(5000);
-            urlConnection.setUseCaches(false);
-            urlConnection.setDoOutput(true); // indicates POST method
-            urlConnection.setDoInput(true);        return false;
-            urlConnection.setReadTimeout(5000);
-            urlConnection.setRequestProperty("Content-Type",
-                    "multipart/form-data; boundary=" + boundary);
-            urlConnection.setRequestProperty("User-Agent", "TerraMobileApp");
-            urlConnection.setRequestProperty("Test", "Bonjour");
-
-            OutputStream outputStream = urlConnection.getOutputStream();
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(outputStream), true);
-
-            addFormField("fileName",originFile.getName(), writer, outputStream);
-
-            addFilePart("file", originFile, writer, outputStream);
-
-            finish(writer, urlConnection);*/
-/*
-            return true;
-        }catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }catch (IllegalArgumentException e){
-            e.printStackTrace();
-            return false;
-        }*/
         } catch (ClientProtocolException e) {
             e.printStackTrace();
             return false;
@@ -181,31 +132,4 @@ public class UploadTask extends AsyncTask<String, String, Boolean> {
         mainActivity.getProgressDialog().dismiss();
         Message.showSuccessMessage(mainActivity, R.string.success, R.string.download_cancelled);
     }
-
-    private List<String> finish(PrintWriter writer, HttpURLConnection conn) throws IOException {
-        List<String> response = new ArrayList<String>();
-
-        writer.append(LINE_FEED).flush();
-        writer.append("--" + boundary + "--").append(LINE_FEED);
-        writer.close();
-
-        // checks server's status code first
-        int status = conn.getResponseCode();
-        if (status == HttpURLConnection.HTTP_OK) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    conn.getInputStream()));
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                response.add(line);
-            }
-            reader.close();
-            conn.disconnect();
-
-        } else {
-            throw new IOException("Server returned non-OK status: " + status);
-        }
-        onPostExecute();
-        return response;
-    }
-
 }
